a90c58dec75e0cbdaa0b29e1d4b8a6f731097f73
斯比尔特 <spirit12@qq.com>
2025-02-10 19:45:08 +0800

    <feature> 红点和事件系统的游戏工程上传

---
+++ Assets/Game/Scripts/RedTip/RedTipConst.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using DCFrame;
+using System.Collections.Generic;
+
+namespace Game {
+    public class RedTipConst {
+        /// <summary>
+        /// 游戏红点常量初始化调用
+        /// </summary>
+        public static void Initialization() {
+            foreach (var keyValue in redTipTreeDic) {
+                RedTipTree.redTipTreeDic.Add(keyValue.Key, keyValue.Value);
+            }
+            foreach (var keyValue in redTipBaseDic) {
+                RedTipTree.redTipBaseDic.Add(keyValue.Key, keyValue.Value);
+            }
+        }
+
+        #region 红点常量
+
+        public const string RedTipMain = nameof(RedTipMain);
+
+        #endregion
+
+        #region 红点树字典
+
+        private static readonly Dictionary<string, List<string>> redTipTreeDic = new Dictionary<string, List<string>>() { };
+
+        #endregion
+
+        #region 红点实例化获取
+        private static readonly Dictionary<string, Func<RedTipBase>> redTipBaseDic = new Dictionary<string, Func<RedTipBase>>() { };
+
+        #endregion
+    }
+}
+
